c68b77d9577691cbd554f472470cd6c05c8bfa23
Sopan Bharkad <131639162+Sopan7654@users.noreply.github.com>
2025-04-01 12:36:03 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by ₷𝖔𝙥𝘢𝙣</p>
+                <p>Made with by ₷𝖔𝙥𝘢𝙣</p>
               </div>
             </footer>
           </ThemeProvider>
